622916b3ecaaa2963381685e2ced2669bf320ab4
dsaki9205 <67487557+dsaki9205@users.noreply.github.com>
2021-04-12 14:14:35 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import { animals } from './animals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const title = "";
+
+const background = (
+  <img
+    className = 'background'
+    alt='ocean'
+    src = '/images/ocean.jpg'
+ />
+ )
+
+const animalFacts =( 
+  <div>
+  <h1> {title == "" ? "Click an animal for a fun fact" : title}</h1>
+  </div>
+  )
+ReactDOM.render(animalFacts, document.getElelemtById("root"));
